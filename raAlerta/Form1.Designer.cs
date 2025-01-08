854c9c169439dd5d25857191e09bec3d13eed13b
Brunoopado <brunogodoypadovani@gmail.com>
2025-01-07 22:53:38 -0300

    First

---
+++ raAlerta/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace raAlerta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnContato = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtMsg2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnContato
+            // 
+            this.btnContato.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnContato.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnContato.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnContato.Location = new System.Drawing.Point(318, 651);
+            this.btnContato.Name = "btnContato";
+            this.btnContato.Size = new System.Drawing.Size(161, 56);
+            this.btnContato.TabIndex = 9;
+            this.btnContato.Text = "ENTRAR EM CONTATO";
+            this.btnContato.UseVisualStyleBackColor = true;
+            this.btnContato.Click += new System.EventHandler(this.btnContato_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.BackColor = System.Drawing.SystemColors.Desktop;
+            this.txtMsg.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMsg.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMsg.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtMsg.Location = new System.Drawing.Point(29, 374);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(450, 22);
+            this.txtMsg.TabIndex = 10;
+            // 
+            // txtMsg2
+            // 
+            this.txtMsg2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.txtMsg2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMsg2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMsg2.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtMsg2.Location = new System.Drawing.Point(29, 449);
+            this.txtMsg2.Multiline = true;
+            this.txtMsg2.Name = "txtMsg2";
+            this.txtMsg2.Size = new System.Drawing.Size(450, 47);
+            this.txtMsg2.TabIndex = 11;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(33, 502);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 17);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "UM DIA";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(33, 545);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(97, 17);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "UMA SEMANA";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(33, 591);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(68, 17);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "UM MÊS";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.BackColor = System.Drawing.Color.Lime;
+            this.btnOk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnOk.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.ForeColor = System.Drawing.Color.Green;
+            this.btnOk.Location = new System.Drawing.Point(33, 651);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 56);
+            this.btnOk.TabIndex = 15;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = false;
+            this.btnOk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::raAlerta.Properties.Resources.exit_svgrepo_com;
+            this.pictureBox2.Location = new System.Drawing.Point(451, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::raAlerta.Properties.Resources.ra_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(42, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(412, 345);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(500, 750);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.txtMsg2);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnContato);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(500, 500);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 750);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 750);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnContato;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.TextBox txtMsg2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ raAlerta/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace raAlerta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            SetStartup();
+
+            if (!File.Exists(Application.StartupPath + @"\dados.txt"))
+                File.WriteAllText(Application.StartupPath + @"\dados.txt",DateTime.Now.AddMonths(6).ToString());
+
+            if (DateTime.Parse(File.ReadAllText(Application.StartupPath + @"\dados.txt")) >= DateTime.Now)
+                return;
+            else
+                Environment.Exit(0);
+
+            string tempoEspera = File.ReadAllText(Application.StartupPath + @"\dados.txt");
+
+            if (tempoEspera == "NUNCA")
+            {
+                return;
+            }
+            else
+            {
+                txtMsg.Text = "Já se passaram mais de 6 meses desde a última manutenção!";
+                txtMsg2.Text = "Para manter sua máquina em perfeito estado, entre em contato para uma revisão!";
+                this.StartPosition = FormStartPosition.Manual;
+                Rectangle screen = Screen.PrimaryScreen.WorkingArea;
+
+                for (int i = screen.Height; i > screen.Height - this.Height; i = i - 13)
+                {
+                    this.Location = new Point(screen.Width - this.Width, i);
+                    await Task.Delay(1);
+                }
+                this.Location = new Point(screen.Width - this.Width, screen.Height - this.Height);
+            }
+        }
+        public static void SetStartup()
+        {
+            try
+            {
+                using (RegistryKey startupKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+                {
+                    if (startupKey == null)
+                    {
+                        return;
+                    }
+                    string appName = "RaAlerta";
+                    string appPath = "\"" + Application.ExecutablePath + "\"";
+
+                    object existingValue = startupKey.GetValue(appName);
+                    if (existingValue != null && existingValue.ToString() == appPath)
+                    {
+                        return;
+                    }
+                    startupKey.SetValue(appName, appPath);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Permissões administrativas são necessárias para realizar essa operação.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}");
+            }
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnContato_Click(object sender, EventArgs e)
+        {
+            string url = "https://api.whatsapp.com/send/?phone=551733455001&text=Ol%C3%A1%2C+R.A+Inform%C3%A1tica%21+Gostaria+de+agendar+uma+revis%C3%A3o+para+o+meu+PC%2Fnotebook.+Obrigado%21&type=phone_number&app_absent=0";
+
+            
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selectedRadioButton = this.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked).Text;
+            string path = Application.StartupPath + @"\dados.txt";
+
+            if (selectedRadioButton != null)
+            {
+                switch (selectedRadioButton)
+                {
+                    case "UM DIA":
+                        File.WriteAllText(path, DateTime.Now.AddDays(1.0).ToString());
+                        break;
+                    case "UMA SEMANA":
+                        File.WriteAllText(path, DateTime.Now.AddDays(7.0).ToString());
+                        break;
+                    case "UM MÊS":
+                        File.WriteAllText(path, DateTime.Now.AddMonths(1).ToString());
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Defina quando quer ser lembrado!");
+            }
+            this.Close();
+        }
+    }
+}
